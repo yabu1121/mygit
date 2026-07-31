2690993f3b09dfce2c5dcac8d0de9f4a60be80e2
yabu1121 <n.hayato0122@outlook.jp>
2026-07-31 18:28:27 +0900

    mygit init

--- internal/commands/init.go
+++ internal/commands/init.go
@@ -1,8 +1,33 @@
 package commands
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
 
 func Init() error {
-	fmt.Println("init called")
+	gitDir := ".mygit"
+
+	directories := []string{
+		filepath.Join(gitDir, "objects"),
+		filepath.Join(gitDir, "refs", "heads"),
+	}
+
+	for _, directory := range directories {
+		if err := os.MkdirAll(directory, 0755); err != nil {
+			return fmt.Errorf("create directory %s: %w", directory, err)
+		}
+	}
+
+	headPath := filepath.Join(gitDir, "HEAD")
+	headContent := []byte("ref: refs/heads/main\n")
+
+	if err := os.WriteFile(headPath, headContent, 0644); err != nil {
+		return fmt.Errorf("write HEAD: %w", err)
+	}
+
+	fmt.Printf("Initialized empty Mygit repository in %s\n", gitDir)
+
 	return nil
 }
